d226160dff982757766f855e9099c16d3e8e9cb1
Robertt <roberttsreis00@gmail.com>
2025-08-27 14:32:43 -0300

    Funcao gerar ordem de compra

--- core/tasks.py
+++ core/tasks.py
@@ -75,74 +75,74 @@ def tratar_sugestao_de_compras(self, file_id):
             'Número da ordem de compra': [],  # Pode ter isso no BD ORDEM 1 e vai acrescentando
             'Código (SKU)': [],
         }
+        with task_instance.Workbook.open('rb') as f:
+            sugestao_compras_excel = pd.read_excel(f)
 
-        sugestao_compras_excel = pd.read_excel(task_instance.Workbook)
+            sk_us = sugestao_compras_excel.iloc[0:, 0]
 
-        sk_us = sugestao_compras_excel.iloc[0:, 0]
+            if task_instance.situacao_compra == 'Giro':
+                sugestao_compras = sugestao_compras_excel.iloc[:, 5]
+                ajuste_comprador = sugestao_compras_excel.iloc[:, 6]
+            else:
+                sugestao_compras = sugestao_compras_excel.iloc[:, 7]
+                ajuste_comprador = sugestao_compras_excel.iloc[:, 8]
 
-        if task_instance.situacao_compra == 'Giro':
-            sugestao_compras = sugestao_compras_excel.iloc[:, 5]
-            ajuste_comprador = sugestao_compras_excel.iloc[:, 6]
-        else:
-            sugestao_compras = sugestao_compras_excel.iloc[:, 7]
-            ajuste_comprador = sugestao_compras_excel.iloc[:, 8]
+            qtd_de_skus = len(sk_us)
+            cont = 1
 
-        qtd_de_skus = len(sk_us)
-        cont = 1
+            for x, y, z in zip(sk_us, sugestao_compras, ajuste_comprador):
+                comprado = z
 
-        for x, y, z in zip(sk_us, sugestao_compras, ajuste_comprador):
-            comprado = z
+                if str(z) == 'nan':
+                    comprado = y
 
-            if str(z) == 'nan':
-                comprado = y
+                if isinstance(x, float):  # quer dizer que é vazio
+                    continue
 
-            if isinstance(x, float):  # quer dizer que é vazio
-                continue
+                if comprado < 0:
+                    continue
 
-            if comprado < 0:
-                continue
+                consulta_sku = consultar_sku(x)
+                print(f'Pesquisando: {cont}/{qtd_de_skus}')
+                cont += 1
 
-            consulta_sku = consultar_sku(x)
-            print(f'Pesquisando: {cont}/{qtd_de_skus}')
-            cont += 1
+                work_new_compras['ID'].append('')
+                work_new_compras['DATA'].append(data_atual)
+                work_new_compras['ID contato'].append('')
+                work_new_compras['Nome do contato'].append(task_instance.Fornecedor)
+                work_new_compras['Desconto'].append('')
+                work_new_compras['Observações'].append('')
+                work_new_compras['Situação'].append('Em Aberto')
+                work_new_compras['ID Produto'].append(consulta_sku['id'])
+                work_new_compras['Descrição'].append(consulta_sku['nome'])
+                work_new_compras['Quantidade'].append(comprado)
+                work_new_compras['Valor Custo'].append(consulta_sku['preco_custo'])
+                work_new_compras['Número da ordem de compra'].append(ordem_compra)
+                work_new_compras['Código (SKU)'].append(x)
 
-            work_new_compras['ID'].append('')
-            work_new_compras['DATA'].append(data_atual)
-            work_new_compras['ID contato'].append('')
-            work_new_compras['Nome do contato'].append(task_instance.Fornecedor)
-            work_new_compras['Desconto'].append('')
-            work_new_compras['Observações'].append('')
-            work_new_compras['Situação'].append('Em Aberto')
-            work_new_compras['ID Produto'].append(consulta_sku['id'])
-            work_new_compras['Descrição'].append(consulta_sku['nome'])
-            work_new_compras['Quantidade'].append(comprado)
-            work_new_compras['Valor Custo'].append(consulta_sku['preco_custo'])
-            work_new_compras['Número da ordem de compra'].append(ordem_compra)
-            work_new_compras['Código (SKU)'].append(x)
+                sleep(0.6)
 
-            sleep(0.6)
+            df_processado = pd.DataFrame(work_new_compras)
+            original_filename_base, original_filename_ext = os.path.splitext(task_instance.Workbook.name)
+            output_filename = f"processed_{os.path.basename(original_filename_base)}_{task_instance.id}.xlsx"
 
-        df_processado = pd.DataFrame(work_new_compras)
-        original_filename_base, original_filename_ext = os.path.splitext(task_instance.Workbook.name)
-        output_filename = f"processed_{os.path.basename(original_filename_base)}_{task_instance.id}.xlsx"
+            # Cria um caminho temporário para salvar o arquivo processado antes de salvá-lo no FileField
+            temp_output_path = os.path.join('/tmp', output_filename)  # Usar /tmp para arquivos temporários no Docker
 
-        # Cria um caminho temporário para salvar o arquivo processado antes de salvá-lo no FileField
-        temp_output_path = os.path.join('/tmp', output_filename)  # Usar /tmp para arquivos temporários no Docker
+            # Salva o DataFrame processado em um novo arquivo Excel
+            df_processado.to_excel(temp_output_path, index=False)
 
-        # Salva o DataFrame processado em um novo arquivo Excel
-        df_processado.to_excel(temp_output_path, index=False)
+            # Salva o arquivo de saída no modelo ArquivosProcessados
+            with open(temp_output_path, 'rb') as f_output:
+                task_instance.output_file.save(output_filename, ContentFile(f_output.read()), save=True)
 
-        # Salva o arquivo de saída no modelo ArquivosProcessados
-        with open(temp_output_path, 'rb') as f_output:
-            task_instance.output_file.save(output_filename, ContentFile(f_output.read()), save=True)
+            # Remove o arquivo temporário após salvar no modelo
+            os.remove(temp_output_path)
 
-        # Remove o arquivo temporário após salvar no modelo
-        os.remove(temp_output_path)
+            task_instance.status = 'Completo'
+            task_instance.save()
 
-        task_instance.status = 'Completo'
-        task_instance.save()
-
-        return {'status': 'success', 'output_file_url': task_instance.output_file.url}
+            return {'status': 'success', 'output_file_url': task_instance.output_file.url}
 
     except ArquivosProcessados.DoesNotExist:
         print(f"Erro: Instância de ArquivosProcessados com ID {file_id} não encontrada.")
