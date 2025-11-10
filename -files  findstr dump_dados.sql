cf3c29d5f14ec09e44f90127d048d5324f062956
Robertt <roberttsreis00@gmail.com>
2025-11-10 17:44:49 -0300

    Funcao de calcular lucro liquido

---
+++ -files  findstr dump_dados.sql
@@ -0,0 +1,66 @@
+[33mfde72bc[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m Funcao de calcular lucro liquido
+[33m54ca32c[m Funcao de calcular lucro liquido
+[33m4bfdf24[m[33m ([m[1;31morigin/master[m[33m)[m Funcao de calcular lucro liquido
+[33m0b9084d[m Funcao de calcular lucro liquido
+[33md4cb49a[m Funcao de calcular lucro liquido
+[33m3f54014[m Funcao de calcular lucro liquido
+[33m1e4646e[m Funcao de calcular lucro liquido
+[33mbeca61f[m Funcao de calcular lucro liquido
+[33m7615d45[m Funcao de calcular lucro liquido
+[33mb07e8ee[m dados novos
+[33m34eed4b[m dados ate dezembro
+[33m610db87[m Quantidade estoque
+[33md0b9314[m banco de dados até abril
+[33mf65e7e6[m ajustes nas datas de comparação
+[33m1e475d7[m ajustes nas datas de comparação
+[33m4b054e4[m ajustes nas datas de comparação
+[33m37d31cc[m ultimos dias, frete, banco de dados novo
+[33medc7d09[m grafico de periodo anterior
+[33m1f7e3a5[m forms loop
+[33mf644c32[m Concluindo merge com origin/master
+[33m391762f[m Concluindo merge com origin/master
+[33m196166b[m Requirements
+[33med654fa[m Merge branch 'master' of github.com:roberttreis00/andareintranet
+[33mf9f4959[m Removendo db.sqlite3 do versionamento
+[33m3b9515d[m Removendo db.sqlite3 do versionamento
+[33m3cd94b7[m ignorar banco
+[33mc38c13a[m ignorar banco
+[33m4f98450[m codigos
+[33mdac6226[m pesquisar custos
+[33m9dc287f[m upp 17 09 graficos mais banco de dados
+[33m234c1cf[m curva abc mais graficos e css
+[33md644394[m atualizacoes agora filtra por marca
+[33m56530ca[m Dashboard
+[33m84a1389[m arquivos
+[33m2b3fe30[m Funcao gerar ordem de compra
+[33m0648e5b[m Funcao gerar ordem de compra
+[33m6cc682d[m Funcao gerar ordem de compra
+[33md5f2b99[m Funcao gerar ordem de compra
+[33m60e2484[m Funcao gerar ordem de compra
+[33mff20ad7[m Funcao gerar ordem de compra
+[33ma52028c[m Funcao gerar ordem de compra
+[33m86bb1b9[m Funcao gerar ordem de compra
+[33mb362529[m Funcao gerar ordem de compra
+[33m98c2da2[m Funcao gerar ordem de compra
+[33m96c918f[m Funcao gerar ordem de compra
+[33mf9201b2[m Funcao gerar ordem de compra
+[33m745cdad[m Banco ativos
+[33m73c8090[m Funcao gerar ordem de compra
+[33m53f9fea[m Funcao gerar ordem de compra
+[33m30f99ab[m Funcao gerar ordem de compra
+[33m787d447[m sugestao programada
+[33m8ea9d6e[m Uma alteração no CSS, Validar planilha inserida por nome,testes OK
+[33mb949ca2[m Tirando a contagem de uns envio FULL duplicado
+[33ma1359d6[m eliminar das saidas o FULL SBS
+[33mcffcb3b[m att
+[33m8ec21d4[m configuração docker
+[33m4d4a828[m configuração docker
+[33mc14a8c2[m configuração docker
+[33m1986fba[m configuração docker
+[33mcb80d66[m configuração docker
+[33m68305fa[m configuração docker
+[33m8e40538[m configuração docker
+[33m562c242[m configuração docker
+[33m2bedcab[m configuração docker
+[33m8cf0649[m Projeto andareintranet
+[33m306f387[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Initial commit
